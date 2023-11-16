a10eff748f31b48d53e043d4fdc343528efb2513
Ying Lu <yinglu5374@gmail.com>
2023-11-15 22:59:06 -0500

    update css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
-import Image from 'next/image'
-
 // http://localhost:3000/
 const Home = () => {
-  return (
-    <div>
-      <h2>Learn Nextjs</h2>
-      <p>
-        For more information, see <a href='https://nextjs.org/'>nextjs.org</a>
-      </p>
-    </div>
-  )
+  return <h1>Hello World</h1>
 }
 
 export default Home
